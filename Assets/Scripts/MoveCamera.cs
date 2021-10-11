c5c4c340827cbaea23b79ee60d8638f0f78a7873
nohell <noel.hmnn@gmail.com>
2021-10-11 18:19:10 +0200

    Slight Char Changes

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour {
 
-    public Transform player;
+    public GameObject player;
 
     void Update() {
         transform.position = player.transform.position;
